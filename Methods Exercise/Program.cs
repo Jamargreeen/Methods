8f1771afceb6f47c6a6007a83ec8e0eff75db5a6
Jamar Green <jamargreen22@gmail.com>
2021-12-21 18:14:19 -0600

    initial commit

---
+++ Methods Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods_Exercise
+{
+	class Program
+	{
+		public static int Add(int num1, int num2)
+		{
+			return num1 + num2;
+		}
+
+		public static decimal Add(decimal num1, decimal num2)
+		{
+			return num1 + num2;
+		}
+
+		public static string Add(int num1, int num2, bool isCheck)
+		{
+			var sum = num1 + num2;
+
+			if (isCheck == true && sum > 1)
+			{
+				return $"{sum} dollars";
+			}
+			else if (isCheck == true && sum == 1)
+			{
+				return $"{sum} dollar";
+			}
+			else if (isCheck == true && sum < 1)
+			{
+				return $"{sum} dollars";
+			}
+			else
+			{
+				return sum.ToString();
+			}
+		}
+
+	}
+}	
